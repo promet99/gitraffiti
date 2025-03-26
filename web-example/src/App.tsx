15f7a0f17b30c0968f66b7b64732caf5dfa11cd9
promet99 <54811538+promet99@users.noreply.github.com>
2025-03-26 22:37:33 +0900

    feat: init

---
+++ web-example/src/App.tsx
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div>
+        <button className="btn btn-primary">Primary</button>
+        <button className="btn btn-secondary">Secondary</button>
+        <button className="btn btn-accent">Accent</button>
+      </div>
+    </>
+  );
+}
+
+export default App;
